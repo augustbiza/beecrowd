41c81984a9fc2f316c88541828c0b96b0970a907
Augusto Bizarria <agbiza1919@gmail.com>
2024-10-28 12:15:58 -0300

    sequence of numbers and sum

--- Java/Beginner/1101/Main.java
+++ Java/Beginner/1101/Main.java
@@ -6,32 +6,35 @@ class Main {
     public static void main(String []args) throws IOException {
         Scanner scan = new Scanner(System.in);
 
-        int m = scan.nextInt(), n = scan.nextInt(), higher, lower, sum = 0;
+        int m, n;
 
-        while(m > 0 && n > 0) {
+        do{
+
+            m = scan.nextInt();
+            n = scan.nextInt();
+            
+            int higher, lower, sum = 0;
 
             if(m > 0 && n > 0) {
 
-            if(m > n) { 
-                higher = m;
-                lower = n;
-            }
-            else {
-                higher = n;
-                lower = m;
+                if(m > n) { 
+                    higher = m;
+                    lower = n;
+                }
+                else {
+                    higher = n;
+                    lower = m;
+                }
+
+                for(int i = lower; i <= higher; i++) {
+                    sum += i;
+                    System.out.printf("%d ", i);
+                }
+
+                System.out.printf("Sum=%d\n", sum);
             }
 
-            for(int i = lower; i <= higher; i++) {
-                sum += i;
-                System.out.printf("%d ", i);
-            }
-
-            System.out.printf("Sum=%d\n", sum);
-            }
-
-            m = scan.nextInt(); n = scan.nextInt(); sum = 0;
-
-        }
+        }while(m > 0 && n > 0);
 
         scan.close();
     }
