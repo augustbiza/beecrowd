8294a68f7408b2757e97606a7cd36da78ff43583
Augusto Bizarria <agbiza1919@gmail.com>
2024-12-02 00:42:11 -0300

    difference

---
+++ Cpp/Beginner/1007/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(void) {
+
+    int a, b, c, d, dif;
+    cin >> a >> b >> c >> d;
+
+    dif = a*b - c*d;
+
+    cout << "DIFERENCA = " << dif << endl;
+}
