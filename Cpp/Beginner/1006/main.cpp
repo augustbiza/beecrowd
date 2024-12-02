1283a01bfa8bf7901195fc03b69867d6e14fa236
Augusto Bizarria <agbiza1919@gmail.com>
2024-12-02 00:40:57 -0300

    average 2

---
+++ Cpp/Beginner/1006/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(void) {
+
+    float a, b, c, media;
+    cin >> a >> b >> c;
+
+    media = (a*2 + b*3 + c*5) / 10;
+
+    cout << fixed << setprecision(1);
+
+    cout << "MEDIA = " << media << endl;
+}
