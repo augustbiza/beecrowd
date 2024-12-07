da94dd38b0311686d047e6ce39a31e02999dd537
Augusto Bizarria <agbiza1919@gmail.com>
2024-12-07 15:59:43 -0300

    distance

---
+++ Cpp/Beginner/1016/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+    int km, min;
+    cin >> km;
+
+    min = km * 2;   //it can get away one kilometer for each 2 minutes
+
+    cout << min << " minutos" << endl;
+}
