61c2b327009d53c8eb35a215aca530b89460d35d
Augusto Bizarria <agbiza1919@gmail.com>
2025-03-29 07:23:30 -0300

    1005

--- 1-Beginner/1004/bee1004.java
+++ 1-Beginner/1004/bee1004.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 class bee1004 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner (System.in);
 
         int a, b, prod;
